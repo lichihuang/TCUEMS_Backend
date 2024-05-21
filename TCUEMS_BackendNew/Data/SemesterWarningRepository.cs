bde7ac77425679eb7cfa4565beeaaa202497d673
Li Chi Huang <71595575+lichihuang@users.noreply.github.com>
2024-05-21 08:13:17 +0800

    Update SemesterWarningRepository.cs

--- TCUEMS_BackendNew/Data/SemesterWarningRepository.cs
+++ TCUEMS_BackendNew/Data/SemesterWarningRepository.cs
@@ -1,9 +1,9 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.Extensions.Logging;
 using TCUEMS_BackendNew.Models;
 
 namespace TCUEMS_BackendNew.Data
@@ -11,10 +11,12 @@ namespace TCUEMS_BackendNew.Data
     public class SemesterWarningRepository : ISemesterWarningRepository
     {
         private readonly string _connectionString;
+        private readonly ILogger<SemesterWarningRepository> _logger;
 
-        public SemesterWarningRepository(string connectionString)
+        public SemesterWarningRepository(string connectionString, ILogger<SemesterWarningRepository> logger)
         {
             _connectionString = connectionString;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<SemesterWarning>> GetAllSemesterWarnings()
@@ -24,41 +26,40 @@ namespace TCUEMS_BackendNew.Data
             return await dbConnection.QueryAsync<SemesterWarning>("SELECT * FROM Semester_Warning_View");
         }
 
-        public async Task<IEnumerable<SemesterWarning>> GetWarningsByStudentId(int studentId)
-        {
-            using IDbConnection dbConnection = new SqlConnection(_connectionString);
-            dbConnection.Open();
-            return await dbConnection.QueryAsync<SemesterWarning>("SELECT * FROM Semester_Warning_View WHERE W_std_no = @studentId", new { studentId });
-        }
-
         public async Task<IEnumerable<SemesterWarning>> GetSemesterWarningsByCriteria(SemesterWarning criteria)
         {
             using SqlConnection dbConnection = new SqlConnection(_connectionString);
             await dbConnection.OpenAsync();
 
             string sqlQuery = "SELECT * FROM Semester_Warning_View WHERE 1 = 1";
-
             var queryParams = new DynamicParameters();
 
             if (!string.IsNullOrEmpty(criteria.w_dept_no))
             {
-                sqlQuery += " AND W_dept_no = @DeptNameS";
-                queryParams.Add("@DeptNameS", criteria.w_dept_no);
+                sqlQuery += " AND w_dept_no = @DeptNo";
+                queryParams.Add("@DeptNo", criteria.w_dept_no);
             }
 
             if (!string.IsNullOrEmpty(criteria.w_smtr))
             {
-                sqlQuery += " AND W_smtr = @WSmtr";
+                sqlQuery += " AND w_smtr = @WSmtr";
                 queryParams.Add("@WSmtr", criteria.w_smtr);
             }
 
             if (!string.IsNullOrEmpty(criteria.w_std_no))
             {
-                sqlQuery += " AND W_std_no = @WStdNo";
+                sqlQuery += " AND w_std_no = @WStdNo";
                 queryParams.Add("@WStdNo", criteria.w_std_no);
             }
 
-            return await dbConnection.QueryAsync<SemesterWarning>(sqlQuery, queryParams);
+            _logger.LogInformation("Generated SQL Query: {SqlQuery}", sqlQuery);
+            foreach (var param in queryParams.ParameterNames)
+            {
+                _logger.LogInformation("Parameter: {ParameterName} = {ParameterValue}", param, queryParams.Get<object>(param));
+            }
+
+            var results = await dbConnection.QueryAsync<SemesterWarning>(sqlQuery, queryParams);
+            return results;
         }
     }
 }
