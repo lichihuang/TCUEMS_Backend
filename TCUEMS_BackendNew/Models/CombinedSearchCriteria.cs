2cc486f576a8c9b5a88359e272a9e7caf36c20d0
Li Chi Huang <71595575+lichihuang@users.noreply.github.com>
2024-05-21 16:56:09 +0800

    Add files via upload

---
+++ TCUEMS_BackendNew/Models/CombinedSearchCriteria.cs
@@ -0,0 +1,10 @@
+﻿namespace TCUEMS_BackendNew.Models
+{
+    public class CombinedSearchCriteria
+    {
+        public string w_dept_no { get; set; }
+        public string w_smtr { get; set; }
+        public int? w_course { get; set; }
+        public int? w_required_course { get; set; }
+    }
+}
