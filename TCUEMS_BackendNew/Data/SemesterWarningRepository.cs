a66e8f9b3ea3333bdb7eb5180f69a20758c2538f
Li Chi Huang <71595575+lichihuang@users.noreply.github.com>
2024-05-24 08:52:21 +0800

    Update SemesterWarningRepository.cs

--- TCUEMS_BackendNew/Data/SemesterWarningRepository.cs
+++ TCUEMS_BackendNew/Data/SemesterWarningRepository.cs
@@ -10,15 +10,17 @@ namespace TCUEMS_BackendNew.Data
 {
     public class SemesterWarningRepository : ISemesterWarningRepository
     {
-        private readonly string _connectionString;
-        private readonly ILogger<SemesterWarningRepository> _logger;
+        private readonly string _connectionString; // 定義資料庫連接字串
+        private readonly ILogger<SemesterWarningRepository> _logger; // 定義日誌記錄介面
 
+        // 注入資料庫連接字串和日誌記錄
         public SemesterWarningRepository(string connectionString, ILogger<SemesterWarningRepository> logger)
         {
-            _connectionString = connectionString;
-            _logger = logger;
+            _connectionString = connectionString; // 初始化資料庫連接字串
+            _logger = logger; // 初始化日誌記錄
         }
 
+        // 異步方法取得所有學期警告
         public async Task<IEnumerable<SemesterWarning>> GetAllSemesterWarnings()
         {
             using IDbConnection dbConnection = new SqlConnection(_connectionString);
@@ -26,11 +28,13 @@ namespace TCUEMS_BackendNew.Data
             return await dbConnection.QueryAsync<SemesterWarning>("SELECT * FROM Semester_Warning_View");
         }
 
+        // 異步方法根據條件取得學期警告
         public async Task<IEnumerable<SemesterWarning>> GetSemesterWarningsByCriteria(SemesterWarning criteria)
         {
             using SqlConnection dbConnection = new SqlConnection(_connectionString);
             await dbConnection.OpenAsync();
 
+            // L查詢語句
             string sqlQuery = "SELECT * FROM Semester_Warning_View WHERE 1 = 1";
             var queryParams = new DynamicParameters();
 
@@ -52,6 +56,8 @@ namespace TCUEMS_BackendNew.Data
                 queryParams.Add("@WStdNo", criteria.w_std_no);
             }
 
+
+            // 記錄
             _logger.LogInformation("Generated SQL Query: {SqlQuery}", sqlQuery);
             foreach (var param in queryParams.ParameterNames)
             {
